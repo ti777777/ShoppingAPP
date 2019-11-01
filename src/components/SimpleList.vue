317b4f99cbd6034217285a00ebf2cabb8da853c1
ti777777 <ti122721986@yahoo.com.tw>
2019-11-01 09:14:44 +0800

    create a responsive layout page

---
+++ src/components/SimpleList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ul>
+            <li>123456</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props:[
+    ]
+}
+</script>
+<style scoped>
+    
+</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="tile">
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.tile{
+    width:100%;
+    height:300px;
+    position:relative;
+    background-color: #fff;
+    border:1px solid;
+    box-sizing: border-box;
+}
+.tile-header{
+    width: 100%;
+    background-color: #eee;
+    border: 1px solid #bbb;
+}
+.tile-body{
+    width: 100%;
+}
+.tile-footer{
+    width: 100%;
+    background-color: #eee;
+    border: 1px solid #bbb;
+}
+
+</style>
