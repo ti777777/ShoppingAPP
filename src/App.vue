eb6d663e7b23bbe0f2de4a1178219475e462ded0
ti777777 <ti12272198686@yahoo.com.tw>
2019-11-03 15:40:56 +0800

    tenlong RWD version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app" >
     <div id="nav" class="px-3">
-      <router-link to="/">天</router-link> 
+      <div class="center">
+        <router-link to="/">天</router-link> 
+      </div>
     </div>
-    <div class="outer">
-      <div class="inner">
+    <div class="center">
+      <div class="aside">
+        123456
+      </div>
+      <div class="main">
         <router-view/>
       </div>
     </div>
@@ -27,7 +32,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  max-height:100vh;
+  min-height:100vh;
   margin: 0 auto;
 }
 
@@ -51,49 +56,78 @@ a{
 #nav a.router-link-exact-active {
     text-decoration: none;
 }
+.center{
+  width:1300px;
+  margin:0 auto;
+}
+.aside{
+  width:180px;
+  background-color: pink;
+  float:left;
+}
+.main{
+  width:1110px;
+  background-color: lightblue;
+  float:right;
+}
 
-.outer{
-  background: lightcyan;
+
+@media (max-width:1339px) {
+    .center{
+        width:1230px;
+    }
+    .main{
+      width:1040px;
+    }
 }
-.inner{
-    width: 1280px;
-    height:100vh;
-    margin:0 auto;
-    background: lightskyblue;
-}
-@media (max-width:1280px) {
-    .inner{
+@media (max-width:1279px) {
+    .center{
         width:1100px;
     }
-}
-@media (max-width:1140px){
-    .inner{
-        width:984px;
+    .main{
+      width:910px;
     }
 }
-@media (max-width:1024px){
-    .inner{
-        width:840px;
+@media (max-width:1139px){
+    .center{
+        width:1000px;
+    }
+    .main{
+      width:810px;
+    }
+}
+@media (max-width:1023px){
+    .center{
+        width:860px;
+    }
+    .main{
+      width:670px;
     }
 }
 @media (max-width:880px){
-    .inner{
-        width: 728px;
+    .aside{
+      display: block;
+    }
+    .center{
+        width: 740px;
+    }
+    .main{
+      width:550px;
     }
 }
 @media (max-width: 768px){
-    .inner{
-      width:560px;
+    .aside{
+      display: none;
+    }
+    .center{
+      width:calc(100% - 40px);
+    }
+    .main{
+      width:100%;
     }
 }
 @media (max-width: 600px){
-  .inner{
-    width:440px;
-  }
 }
 @media (max-width:480px){
-  .inner{
-    width: 100vw;
-  }
 }
 </style>
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,28 +1,54 @@
 <template>
     <div class="tile" :style="styling">
-        123 {{ (this.color==undefined)?"r":this.color.c}}
-        {{d}}
+        <div class="tile-body" v-if="this.content.type==1">
+            
+        </div>
+        <div style="background:pink;" v-else-if="this.content.type==2">
+            123
+        </div>
+        <div v-else-if="this.content.type==3">
+
+        </div>
+        <div v-else>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     props:{
-        padding:String,
-        margin:String,
-        height:String,
-        color:{
-            type:Object
+        type:String,
+        //容器設定
+        boxSetting:{
+            type:Object,
+            padding:String,
+            height:String,
+            color:{
+                type:Object
+            },
+            breakpoints:{
+                type:Object
+            },
+            flex:Number,
+            content:String,
+            square:Boolean
+        },
+        //
+        content:{
+            type:Object,
+            contentType:Number
         }
+
     },
     created(){
         window.addEventListener('resize',this.onResize)
+        document.getElementById("b").innerHTML=this.breakpoints||"";
     },
     computed:{
         styling(){
             return {
-                height:this.height,
-               
+                flex:this.flex==null?1:this.flex
             }
         }
     },
@@ -44,8 +70,9 @@ export default {
     width:100%;
     position:relative;
     background-color: #fff;
-    border:1px solid;
-    box-sizing: border-box;
+    box-sizing:border-box;
+    word-break: break-all;
+    padding: 1em;
 }
 .tile-header{
     width: 100%;
@@ -54,6 +81,7 @@ export default {
 }
 .tile-body{
     width: 100%;
+    box-shadow:0 2px 7px #ddd;
 }
 .tile-footer{
     width: 100%;
