9a116549336f2393512bbe0bde9725f18649f2dd
ti777777 <ti122721986@yahoo.com.tw>
2020-07-15 10:26:39 +0800

    修改1.CheckoutList.vue => CartTable.vue 2.加入vuex 使用.state.cartItems儲存購物車資料

---
+++ src/views/Cart.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="text-right">
+        <div class="mt-5 h4 text-center">購物車</div>
+        <CartTable/>
+        <div class="h5">總計{{this.total}}</div>
+        <button @click="checkPayment()" class="rounded-pill px-3 mb-5">結帳</button>
+        <div class="row">
+        </div>
+    </div>
+</template>
+<script>
+import CartTable from '../components/Main/Cart/CartTable.vue'
+export default {
+    computed:{
+        cartItems:function(){
+            return this.$store.state.cartItems;
+        },
+        total:function(){
+            var t=0;
+            this.cartItems.forEach(element => {
+                t+=element.price*element.amount;
+            });
+            return t;
+        }
+    },
+    components:{
+        CartTable
+    },
+    methods:{
+        checkPayment:function(){
+        },
+        checkout:function(){
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
