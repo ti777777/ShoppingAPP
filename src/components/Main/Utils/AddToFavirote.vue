7d4b5270dc01bb6be857ab7b0c1746c68b916e2b
ti777777 <ti122721986@yahoo.com.tw>
2020-07-26 01:29:57 +0800

    add add to favorite button

---
+++ src/components/Main/Utils/AddToFavirote.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="favorite ">
+        <div  class=" display-inline fa fa-star text-light" v-on:click="toggleFavorite">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+        toggleFavorite:function(event){
+            event.target.classList.toggle("text-light");
+            event.target.classList.toggle("text-warning");
+        }
+    }
+}
+</script>
+<style scoped>
+    .favorite{
+        text-shadow: 0 0 2px black;
+    }
+</style>
--- src/components/Main/Utils/ProductBox.vue
+++ src/components/Main/Utils/ProductBox.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="product-box text-center">
+    <div class="product-box text-center ">
+            <div class="img-box overflow-hidden position-relative">
+                <router-link :to=this.url >
+                        <img :src=this.img >
+                </router-link>
+                <Favorite class="favorite"/>
+            </div>
         <router-link :to=this.url >
-            <img :src=this.img >
             <p>{{this.name}}</p>
             <p><b class="text-danger"><i>${{this.price}}</i></b></p>
         </router-link>
     </div>
 </template>
 <script>
+import Favorite from './AddToFavirote'
 export default {
+    components:{
+        Favorite
+    },
     props:[
         'img',
         'url',
@@ -22,10 +31,25 @@ export default {
         width: 200px;
         height:200px;
     }
-    
+    .img-box {
+        border-radius:5px 5px ;
+        width: 200px;
+        height:200px;
+    }
+    img:hover{
+        transform: scale(1.2);
+        transition-duration: 1s;
+    }
+    .favorite{
+        
+        position: absolute;
+        right:10px;
+        bottom: 10px;
+        z-index: 1;
+    }
     @media screen and (max-width : 600px) {
         
-        img{
+        .img-box{
             width: 150px;
         }
     }
